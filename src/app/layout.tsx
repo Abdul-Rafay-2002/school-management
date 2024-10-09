650644427a42274aa5fddf622749232b05655eb2
Abdul Rafay <rafaymalick68@gmail.com>
2024-10-09 22:11:46 +0500

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "School Management Dashboard",
+  description: "Next.js School Management System",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
